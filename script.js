30312f9f1375455451eb8f6c346960ed7ff32e2a
arduinoandraspberrypirocks <138176139+arduinoandraspberrypirocks@users.noreply.github.com>
2024-08-01 19:13:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const board = document.getElementById('board');
 const keyboard = document.getElementById('keyboard');
-const targetWord = 'ATOM';
+const targetWord = 'graph';
 let currentRow = 0;
 let currentCol = 0;
 
