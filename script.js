bbdbd40aafc2bccfc29a4f4c8686fd8198efa887
arduinoandraspberrypirocks <138176139+arduinoandraspberrypirocks@users.noreply.github.com>
2024-08-02 14:50:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ for (let i = 0; i < 30; i++) {
 // Initialize the keyboard
 const keys = 'QWERTYUIOPASDFGHJKLZXCVBNM'.split('');
 keys.forEach(key => {
-    const keyButton = document.querySelector(`.key:contains('${key}')`);
+    const keyButton = document.querySelector(`.key[data-key='${key}']`);
     keyButton.addEventListener('click', () => handleKeyClick(key));
 });
 
